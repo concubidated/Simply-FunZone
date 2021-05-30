57df7b155c8e1b0258731b9c88cde388488bb26d
Horsey <cheval.de.jeanvaljean@gmail.com>
2021-05-29 23:56:32 -0700

    Make highscore character limit configurable
    This commit allows machine operators to select the limit to the number of highscore characters that are selectable for players. The default is 9, but the option to select any number between 4 and 9 is available. I strongly believe that if players are going to use offensive words for their highscore name, they already would be doing so. It only takes 4 letters to type "FUCK", and even if you set the limit to 3, "FCK" is still "offensive". 9 characters should give players a lot more leeway to use more fitting nicknames, and even fit most of first names into a highscore name.
    
    9 is the most characters that would fit comfortably, but with a bit of reclaiming dead space in the theme I can't imagine 20-25 characters being much of a problem (yay full names?)

--- BGAnimations/ScreenNameEntryTraditional underlay/PlayerNameAndDecorations.lua
+++ BGAnimations/ScreenNameEntryTraditional underlay/PlayerNameAndDecorations.lua
@@ -52,7 +52,7 @@ t[#t+1] = LoadFont(ThemePrefs.Get("ThemeFont") == "Common" and "Wendy/_wendy whi
 			or ThemePrefs.Get("ThemeFont") == "Mega" and "Mega/_mega font"
 			or ThemePrefs.Get("ThemeFont") == "Unprofessional" and "Unprofessional/_unprofessional white")..{
 	Name="PlayerName",
-	InitCommand=function(self) self:zoom(ThemePrefs.Get("ThemeFont") ~= "Mega" and 0.75 or 1.22):halign(0):xy(-80,0) end,
+	InitCommand=function(self) self:zoom(ThemePrefs.Get("ThemeFont") ~= "Mega" and 0.6 or 1.22):horizalign(center) end,
 	OnCommand=function(self)
 		self:visible( CanEnterName )
 		self:settext( SL[pn].HighScores.Name or "" )
