f3b0e789e4fe0de3ea38705146215935b5f0054a
Horsey <cheval.de.jeanvaljean@gmail.com>
2023-04-01 22:10:02 -0700

    New "Has Edit" graphic to give more room for song titles
    It's really bothered me that room is made for a "Has Edit" image on the music wheel taking away from the space available to Titles/Subtitles. This commit makes a new image and places it beside the music wheel so that more room is left over for titles so that long titles don't overflow onto this image.
    
    In the future, I'd like to completely remove this icon because it is a very poor workaround to not being able to see ALL of the charts for a particular song. In the future, it would be best to move to an interface system that more closely resembles the default Stepmania theme whereby you can always see the edit charts without having to check with ⬇️ ⬇️ . This however would have to call a lot of UI elements into question, and for the time being, this solves enough of the problems.
     Home

--- Graphics/MusicWheelItem Song NormalPart/default.lua
+++ Graphics/MusicWheelItem Song NormalPart/default.lua
@@ -10,9 +10,9 @@ af[#af+1] = Def.Sprite{
 	Texture=THEME:GetPathG("", "Has Edit (doubleres).png"),
 	InitCommand=function(self)
 		self:horizalign(left):visible(false):zoom(0.375)
-		self:x( _screen.w/(WideScale(2.15, 2.14)) - self:GetWidth()*self:GetZoom() - 8 )
+		self:visible(false):x(-30.5):zoom(0.375)
 
-		if DarkUI() then self:diffuse(0,0,0,1) end
+		-- if DarkUI() then self:diffuse(0,0,0,1) end
 	end,
 	SetCommand=function(self, params)
 		self:visible(params.Song and params.Song:HasEdits(stepstype) or false)
