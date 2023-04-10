098ddcc68d5a72e55629e43a3345ec44aaf9743e
Horsey <cheval.de.jeanvaljean@gmail.com>
2023-04-09 21:37:36 -0700

    Clean up element placements on ScreenEvaluation
    A small redesign of ScreenEvaluation to reduce the amount of "random floating things".
    
    -Difficulty number is now in the coloured box along with the difficulty name (beginner, expert, etc). The difficulty box was also widened from a square to a rectangle to better fit the difficulty name text.
    
    -style (single/double) string was removed from the evaluation screen because it's redundant information when there is a graphical representation of style in the top right of the screen.
    
    -Song credit information is now in a quad that is the same colour as the difficulty box but darkened (ligher colour in rainbow mode).
    
    -Nudged the SongBanner on ScreenEvaluation in Casual mode to sit neatly on the scoring panes.
    
    -Player Mods are left aligned in the x axis, and centered in the y axis; applied a maxHeight so that a long string of mods don't run outside of the visual boundary
    
    -Move Stream Breakdown to an overlay on top of GraphDisplay and add Peak NPS/eBPM/Total Stream since there's room to spare.

--- BGAnimations/ScreenEvaluation common/PerPlayer/Upper/Difficulty.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/Upper/Difficulty.lua
@@ -2,21 +2,35 @@ local player = ...
 local pn = PlayerNumber:Reverse()[player]
 
 return Def.ActorFrame{
+	-- colored square as the background for the difficulty meter
+	Def.Quad{
+		InitCommand=function(self)
+			self:zoomto(40,30)
+			self:y( _screen.cy-71 )
+			self:x(130 * (player==PLAYER_1 and -1 or 1))
+
+			local currentSteps = GAMESTATE:GetCurrentSteps(player)
+			if currentSteps then
+				local currentDifficulty = currentSteps:GetDifficulty()
+				self:diffuse( DifficultyColor(currentDifficulty), true )
+			end
+		end
+	},
 
 	-- difficulty text ("beginner" or "expert" or etc.)
 	LoadFont("Common Normal")..{
 		InitCommand=function(self)
 			self:y(_screen.cy-64)
-			self:x(115 * (player==PLAYER_1 and -1 or 1))
 			self:halign(pn):zoom(0.7)
 
-			local textColor = Color.White
-			local shadowLength = 0
-			if ThemePrefs.Get("RainbowMode") and not HolidayCheer() then
-				textColor = Color.Black
-			end
-			self:diffuse(textColor)
-			self:shadowlength(shadowLength)
+			if player==PLAYER_1 then
+ 				if SL.Global.GameMode == "Casual" then self:x(-130.5) else self:x(-130) end
+ 			elseif player==PLAYER_2 then
+ 				if SL.Global.GameMode == "Casual" then self:x(129.5) else self:x(130) end
+ 			end
+ 			self:horizalign(center):zoom(0.7)
+ 			self:diffuse(Color.Black)
+ 			self:maxwidth(52)
 
 			local style = GAMESTATE:GetCurrentStyle():GetName()
 			if style == "versus" then style = "single" end
@@ -29,31 +43,21 @@ return Def.ActorFrame{
 			local difficulty = ToEnumShortString( steps:GetDifficulty() )
 			difficulty = THEME:GetString("Difficulty", difficulty)
 
-			self:settext( style .. " / " .. difficulty )
-		end
-	},
-
-	-- colored square as the background for the difficulty meter
-	Def.Quad{
-		InitCommand=function(self)
-			self:zoomto(30,30)
-			self:y( _screen.cy-71 )
-			self:x(134.5 * (player==PLAYER_1 and -1 or 1))
-
-			local currentSteps = GAMESTATE:GetCurrentSteps(player)
-			if currentSteps then
-				local currentDifficulty = currentSteps:GetDifficulty()
-				self:diffuse( DifficultyColor(currentDifficulty), true )
-			end
+			--there is no reason to print the style because it is shown graphically in the top right of the screen
+			self:settext( difficulty )
 		end
 	},
 
 	-- numerical difficulty meter
 	LoadFont("Common Bold")..{
 		InitCommand=function(self)
-			self:diffuse(Color.Black):zoom( 0.4 )
-			self:y( _screen.cy-71 )
-			self:x(134.5 * (player==PLAYER_1 and -1 or 1))
+			self:diffuse(Color.Black):zoom( 0.3 )
+  			self:y( _screen.cy-78 )
+ 			if SL.Global.GameMode == "Casual" then
+ 				self:x(130 * (player==PLAYER_1 and -1 or 1))
+ 			else
+ 				self:x(130 * (player==PLAYER_1 and -1 or 1))
+ 			end
 			self:maxwidth(70)
 
 			local meter
