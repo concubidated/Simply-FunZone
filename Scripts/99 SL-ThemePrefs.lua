bd2b86b1b4c7d803bc7c121bd8e731339aa0d13d
Cube <concubidated@gmail.com>
2023-10-19 14:04:06 +0000

    Simply Funzone

--- Scripts/99 SL-ThemePrefs.lua
+++ Scripts/99 SL-ThemePrefs.lua
@@ -16,8 +16,8 @@ end
 
 SL_CustomPrefs.Get = function()
 	 -- emojis are our lingua franca for the 21st century
-	local visualStyleChoices = { "❤", "↖", "🐻", "🦆", "😺", "🎃", "🌈", "⭐", "🤔", "🌀" }
-	local visualStyleValues  = { "Hearts", "Arrows", "Bears", "Ducks", "Cats", "Spooky", "Gay", "Stars", "Thonk", "Technique" }
+	local visualStyleChoices = { "🏰", "❤", "↖", "🎃", "🌈", "⭐", "🤔", "🌀" }
+	local visualStyleValues  = { "FunZone", "Hearts", "Arrows", "Spooky", "Gay", "Stars", "Thonk", "Technique" }
 
 	local year = Year()
 	local month = MonthOfYear()+1
