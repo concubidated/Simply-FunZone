e41686e4e8fc9fff7270366bd5f68e19178d450c
horsey- <cheval.de.jeanvaljean@gmail.com>
2024-03-16 01:40:12 -0700

    remove FA+ from sort menu
    fixes upstream bug
    
    Co-Authored-By: teejusb <5017202+teejusb@users.noreply.github.com>

--- BGAnimations/ScreenSelectMusic overlay/SortMenu/default.lua
+++ BGAnimations/ScreenSelectMusic overlay/SortMenu/default.lua
@@ -284,7 +284,6 @@ local t = Def.ActorFrame {
 		-- present a choice that would allow them to switch to FA+.
 		if SL.Global.Stages.PlayedThisGame == 0 then
 			if SL.Global.GameMode ~= "ITG"      then table.insert(wheel_options, {"ChangeMode", "ITG"}) end
-			if SL.Global.GameMode ~= "FA+"      then table.insert(wheel_options, {"ChangeMode", "FA+"}) end
 			-- Casual players often choose the wrong mode and an experienced player in the area may notice this
 			-- and offer to switch them back to casual mode. This allows them to do so again.
 			-- It's technically not possible to reach the sort menu in Casual Mode, but juuust in case let's still
