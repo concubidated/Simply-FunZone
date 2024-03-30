3e794ba2490e6243e438d0d6c4a8d310e9ef30ec
concubidated <tbrekke@gmail.com>
2024-03-29 18:47:31 -0700

    FZ updates, visualdelay, random song start

--- BGAnimations/ScreenSelectMusic overlay/default.lua
+++ BGAnimations/ScreenSelectMusic overlay/default.lua
@@ -1,3 +1,23 @@
+if GAMESTATE:GetCurrentStageIndex() == 0 then
+	local memoryCardInUse = false
+	for player in ivalues(GAMESTATE:GetHumanPlayers()) do
+		if MEMCARDMAN:GetCardState(player) == "MemoryCardState_ready" then
+			memoryCardInUse = true
+		end
+	end
+
+	if not memoryCardInUse then
+		local t = {
+			"In The Groove 2/Birdie",
+			"In The Groove 2/Bumble Bee",
+			"In The Groove 3/Dance Vibrations",
+			"In The Groove 3/Coming Out",
+		};
+		local s = SONGMAN:FindSong( t[ math.random(1,table.getn(t)) ] )
+		GAMESTATE:SetPreferredSong( s )
+	end
+end
+
 local af = Def.ActorFrame{
 	-- GameplayReloadCheck is a kludgy global variable used in ScreenGameplay in.lua to check
 	-- if ScreenGameplay is being entered "properly" or being reloaded by a scripted mod-chart.
