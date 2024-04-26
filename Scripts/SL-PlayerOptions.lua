f7ca2a02929b75de1518ebad43ed3c5ed6817a6e
concubidated <tbrekke@gmail.com>
2024-04-26 16:19:22 -0700

    Disable TimingWindowReset

--- Scripts/SL-PlayerOptions.lua
+++ Scripts/SL-PlayerOptions.lua
@@ -721,10 +721,7 @@ local Overrides = {
 
 			-- First determine the set of actual enabled windows.
 			local windows = {true,true,true,true,true}
-			local disabledWindows = playeroptions:GetDisabledTimingWindows()
-			for w in ivalues(disabledWindows) do
-				windows[tonumber(ToEnumShortString(w):sub(-1))] = false
-			end
+
 
 			-- Compare them to any of our available selections
 			local matched = false
@@ -746,7 +743,7 @@ local Overrides = {
 			-- to a known value (all windows enabled).
 			if not matched then
 				mods.TimingWindows = {true,true,true,true,true}
-				playeroptions:ResetDisabledTimingWindows()
+				-- playeroptions:ResetDisabledTimingWindows()
 				list[1] = true
 			end
 			return list
@@ -756,10 +753,10 @@ local Overrides = {
 			for i=1,#list do
 				if list[i] then
 					mods.TimingWindows = self.Values[i]
-					playeroptions:ResetDisabledTimingWindows()
+					-- playeroptions:ResetDisabledTimingWindows()
 					for i,enabled in ipairs(mods.TimingWindows) do
 						if not enabled then
-							playeroptions:DisableTimingWindow("TimingWindow_W"..i)
+							-- playeroptions:DisableTimingWindow("TimingWindow_W"..i)
 						end
 					end
 				end
