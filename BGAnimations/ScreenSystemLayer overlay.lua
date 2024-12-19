0ac34749ab21ee3b4b78b0fba08df3a608e9e1f1
horsey- <cheval.de.jeanvaljean@gmail.com>
2024-12-19 15:55:16 -0700

    Add a clock to ScreenSystemLayer
    This adds a clock to various screens that lead from starting gamplay to ScreenSelectMusic. I'd really like to have a more elegant solution for this functionality in the future.

--- BGAnimations/ScreenSystemLayer overlay.lua
+++ BGAnimations/ScreenSystemLayer overlay.lua
@@ -143,7 +143,10 @@ t[#t+1] = Def.BitmapText{
 				self:visible( THEME:GetMetric( screen:GetName(), "ShowCreditDisplay" ) )
 			end
 
-			if PREFSMAN:GetPreference("EventMode") then
+			--we don't want the clock and Event Mode text to overlap
+			--it's possible for machine owners to want to set their machine to EVENT MODE but not HOME MODE
+			--this is useful for public settings where you want to give free continuous plays but don't want to give access to the full ScreenTitleMenu
+			if PREFSMAN:GetPreference("EventMode") and not GAMESTATE:GetCoinMode() == "CoinMode_Home" then
 				self:settext( THEME:GetString("ScreenSystemLayer", "EventMode") )
 
 			elseif GAMESTATE:GetCoinMode() == "CoinMode_Pay" then
@@ -200,8 +203,11 @@ t[#t+1] = Def.BitmapText{
 				self:visible( THEME:GetMetric( screen:GetName(), "ShowCreditDisplay" ) )
 			end
 
-			if PREFSMAN:GetPreference("EventMode") then
-				self:settext( THEME:GetString("ScreenSystemLayer", "EventMode") )
+			--we don't want the clock and Event Mode text to overlap
+			--it's possible for machine owners to want to set their machine to EVENT MODE but not HOME MODE
+			--this is useful for public settings where you want to give free continuous plays but don't want to give access to the full ScreenTitleMenu
+			if PREFSMAN:GetPreference("EventMode") and not GAMESTATE:GetCoinMode() == "CoinMode_Home" then
+				self:settext( THEME:GetString("ScreenSystemLayer", "EventMode") ):spin()
 
 			elseif GAMESTATE:GetCoinMode() == "CoinMode_Pay" then
 				local credits = GetCredits()
@@ -614,5 +620,59 @@ t[#t+1] = Def.ActorFrame {
 	}
 }
 -- -----------------------------------------------------------------------
+
+--Bottom Bar Clock
+t[#t+1] = LoadFont("Common Normal")..{
+	InitCommand=function(self) self:x(_screen.cx):y(SCREEN_BOTTOM-16):zoom(1):horizalign(center) end,
+	OnCommand=function(self) self:playcommand("Refresh") end,
+	ScreenChangedMessageCommand=function(self)
+		self:playcommand("Refresh");
+	end,
+	CoinModeChangedMessageCommand=function(self) self:playcommand("Refresh") end,
+	CoinsChangedMessageCommand=function(self) self:playcommand("Refresh") end,
+	PulseMessageCommand=function(self) self:playcommand("Refresh") end,
+	RefreshCommand=function(self)
+		local screen = SCREENMAN:GetTopScreen()
+		local bShow = true
+		if screen then
+			local sClass = screen:GetName()
+			bShow = THEME:GetMetric( sClass, "ShowCreditDisplay" )
+			 -- hide this centered credit text for certain screens,
+			-- where it would more likely just be distracting and superfluous
+			--I'm leaving out the clock on the player options screens because this screen isn't supposed to be "sat on"
+			if sClass == "ScreenPlayerOptions"
+				or sClass == "ScreenPrompt" -- this screen is an OutFox specific screen that prompts the user to upgrade to the latest OutFox client
+				or sClass == "ScreenTitleMenu"
+				or sClass == "ScreenEditMenu"
+				or sClass == "ScreenEditOptions"
+				or sClass == "ScreenMiniMenuMainMenu"
+				or sClass == "ScreenPlayerOptions2"
+				or sClass == "ScreenEvaluationStage"
+				or sClass == "ScreenEvaluationCourse"
+				or sClass == "ScreenEvaluationSummary"
+				or sClass == "ScreenNameEntryActual"
+				or sClass == "ScreenNameEntryTraditional"
+				or sClass == "ScreenGameOver" then
+				bShow = false
+			end
+		end
+		 --don't show the clock in Free Play or Coin mode, because these modes should have the free play/coins banner
+		if GAMESTATE:GetCoinMode() == "CoinMode_Pay" or GAMESTATE:GetCoinMode() == "CoinMode_Free"
+			then self:visible( false )
+		end
+		 --as long as you are in Home Mode, the clock will be visible on the screens where it's not blacklisted
+		if GAMESTATE:GetCoinMode() == "CoinMode_Home" then
+			self:settext(string.format('%2i:%02i:%02i  %s %02i, %04i', Hour(), Minute(), Second(), MonthToString(MonthOfYear()), DayOfMonth(), Year()))
+			self:visible( bShow )
+		end
+	end,
+}
+ --Pulse by second (used by the clock)
+t[#t+1] = Def.ActorFrame {
+	Def.Quad {
+		PulseCommand=function(self) MESSAGEMAN:Broadcast("Pulse"); self:sleep(1); self:queuecommand("Pulse"); end;
+		InitCommand=function(self) self:visible(false):playcommand("Pulse") end,
+	}
+}
 
 return t
